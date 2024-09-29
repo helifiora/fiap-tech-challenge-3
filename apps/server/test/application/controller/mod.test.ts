d825933623923457947dac6567e5257d19a21733
LuizFmjr <lzfmartinsjr@gmail.com>
2024-09-29 18:20:36 -0300

    Delete apps/server/test/application/controller/mod.test.ts

--- apps/server/test/application/controller/mod.test.ts
+++
@@ -1,7 +0,0 @@
-import { assert, test } from "vitest";
-import * as module from "#application/controller/mod.ts";
-
-test("should define modules", () => {
-  assert.isDefined(module.PostController);
-  assert.isDefined(module.AuthorController);
-});
